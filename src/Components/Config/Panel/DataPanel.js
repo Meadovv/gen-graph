b0cb5eb8b1e548640ebb0a4f09344237ad7ea11f
meadovv <hoangthanh2k3@gmail.com>
2023-12-08 08:40:40 +0700

    new

--- src/Components/Config/Panel/DataPanel.js
+++ src/Components/Config/Panel/DataPanel.js
@@ -1,4 +1,4 @@
-import { Input, Button, Radio, message } from 'antd'
+import { Input, Button, Radio, Tooltip, message } from 'antd'
 import { useEffect, useState } from 'react'
 import { QuestionCircleOutlined } from '@ant-design/icons'
 
@@ -144,7 +144,34 @@ export default function DataPanel({ active, data, setData, setDisableMenu }) {
             }}>
                 <div style={{ marginLeft: 10 }}>
                     Data Format
-                    <QuestionCircleOutlined style={{ marginLeft: 10, cursor: 'pointer' }} />
+                    <Tooltip 
+                        style={{
+                            width: '50%',
+                            height: '100vh'
+                        }}
+                        title={
+                            <div style={{
+                                display: 'flex',
+                                flexDirection: 'column'
+                            }}>
+                                <strong>Data for a graph must comply with the following principles:</strong>
+                                {
+                                    dataFormat === 'wm' ?
+                                    <>
+                                        <span>1. The first line contains an integer <strong>n</strong> that is <strong>the number of vertices in the graph</strong>.</span>
+                                        <span>2. The <strong>next n lines</strong> each contain <strong>exactly n integers</strong>. The number in row <strong>i</strong> and column <strong>j</strong> is <strong>0</strong> if there is no edge connecting vertex i to vertex j in the graph. Conversely, the weight of the edge connecting vertex <strong>i</strong> to vertex <strong>j</strong> is <strong>that number</strong>.</span>
+                                    </> :
+                                    <>
+                                        <span>1. The first line contains two integers <strong>n</strong> and <strong>m</strong> which are the <strong>number of vertices</strong> and <strong>number of edges</strong> of the graph.</span>
+                                        <span>2. The next <strong>m</strong> lines, each line includes 3 integers <strong>u, v, c</strong> represent the edge connecting vertex <strong>u</strong> with vertex <strong>v</strong> with weight <strong>c</strong> on the graph.</span>
+                                    </>
+                                }
+                            </div>
+                        } 
+                        trigger='click'
+                    >
+                        <QuestionCircleOutlined style={{ marginLeft: 10, cursor: 'pointer' }} />
+                    </Tooltip>
                 </div>
                 <Radio.Group value={dataFormat} onChange={(event) => {
                     setDataFormat(event.target.value)
