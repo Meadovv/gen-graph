7e37133ae614f5c55855ea87122b0c6fa4ceacdc
meadovv <hoangthanh2k3@gmail.com>
2023-12-15 13:20:31 +0700

    new

--- src/Components/Graph/GraphView.js
+++ src/Components/Graph/GraphView.js
@@ -1,26 +1,4 @@
-import { Graphviz } from 'graphviz-react';
-import { useEffect, useState } from 'react';
-
 export default function GraphView({ data }) {
-
-    const graphOptions = {
-        fit: false,
-        zoom: true
-    }
-
-    const [dots, setDot] = useState(`graph {
-    }`)
-
-    useEffect(() => {
-        let edgeArray = ''
-        for(let i = 1; i < data.length; ++i) {
-            edgeArray += `${data[i].from} -${data[0].graphMode === 'undirected' ? '-' : '>'} ${data[i].to} [label = ${data[i].weight}, color = black];`
-        }
-        let backupDots = `${data[0].graphMode === 'undirected' ? 'graph' : 'digraph'} {
-            ${edgeArray}
-        }`
-        setDot(backupDots)
-    }, [data])
 
     return (
         <div style={{
@@ -32,10 +10,7 @@ export default function GraphView({ data }) {
             borderRadius: 10,
             border: '1px solid black',
         }}>
-            <Graphviz
-                options={graphOptions} 
-                dot={dots}
-            />
+
         </div>
     )
 }
